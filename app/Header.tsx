14aa625e6b6684c4d0f56d43976df669b2cf3da4
Logan Jones <loganjones9117@gmail.com>
2023-02-03 15:33:11 -0700

    Re-arranged header elements (brought closer to center)

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 function Header() {
   return <header>
-<div className="grid grid-cols-4 p-10 h-8 items-center justify-start gap-5 shadow-md" style={{ display: "flex", justifyContent: "left" }}>
+<div className="grid grid-cols-4 p-8 h-8 items-center justify-center gap-5 shadow-md" style={{ display: "flex", justifyContent: "center" }}>
+        
         <Image className="h-150 w-50 cursor-pointer"
             src = "/AskAnyoneResized-removebg.png"
             width = {250}
@@ -11,17 +12,17 @@ function Header() {
             alt = "AskAnyone"
         />
 
-        <Link href="/" prefetch={false} className="pl-5">
+        <Link href="/" prefetch={false} className="pl-10">
             <h1>Browse Bots</h1>
         </Link>
 
-        <Link href="/" prefetch={false} className="pl-5">
+        <Link href="/" prefetch={false} className="pl-10">
             <h1>Create Bot</h1>
         </Link>
 
         {/* Button Dark Mode */}
 
-        <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800" style={{ marginLeft: "auto" }}>
+        <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-3 rounded-full dark:bg-slate-800" style={{ marginLeft: "300px" }}>
             Contact
         </button>
     </div>
