484ba8e4fa0aefd0769623c6485ddd52f3a7a5b8
Logan Jones <loganjones9117@gmail.com>
2023-02-07 17:57:44 -0700

    refactor: modify csv_to_vectors

--- server/trpc/router/test.py
+++ server/trpc/router/test.py
@@ -11,37 +11,22 @@ dotenv.load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 openai.organization=os.environ["ORG_KEY"]
 
-
-
-pinecone.init(api_key=os.environ["OPENAI_API_KEY"], endpoint="testing-f299968.svc.us-east1-gcp.pinecone.io")
+pinecone.init(api_key=os.environ["PINECONE_API_KEY"], environment="us-east1-gcp")
 # Replace with your Pinecone API key
-api_key = os.environ.get("OPENAI_API_KEY")
 
 # Set the endpoint URL
 url = "testing-f299968.svc.us-east1-gcp.pinecone.io"
 
-# Set the headers for the request
-headers = {
-    "Authorization": "Bearer " + api_key,
-    "Content-Type": "application/json",
-}
-
-
-page_number = []
-vector = []
+MODEL = "text-embedding-ada-002"
 
 batch_size = 1000
-
 def chunks(iterable, batch_size):
     iterator = iter(iterable)
     for first in iterator:
         yield itertools.chain([first], itertools.islice(iterator, batch_size - 1))
 
-MODEL = "text-embedding-ada-002"
-
-
-
-
+page_number = []
+vector = []
 
 def csv_to_vectors(file_name):
     # Open the csv file
@@ -51,19 +36,31 @@ def csv_to_vectors(file_name):
         
         # Skip the header row
         next(reader)
-        
         # Initialize the page_number and vector arrays
         page_number = []
         vector = []
         
-        # Iterate through each row in the csv file
         for row in reader:
+        # Iterate through each row in the csv file
             # Append the page number to the page_number array
-            page_number.append(int(row[0].split(" ")[1]))
             # Append the content to the vector array
-            vector.append(row[1])
+
+            page_number.append(int(row[0].split(" ")[1]))
+            vector.append(row[1])    # Print the contents of both arrays
+    
+    return page_number, vector
 
 
+# pinecone.list_indexes()
 
+page_number, vector  = csv_to_vectors("book.pdf.pages.csv")
+
+
+for i in range(len(page_number)):
+    print("Page number: {}, Content: {}".format(page_number[i], vector[i]))
+
+# Longterm, he couldn't do it.
+# TODO: Upload vectors into pinecone (in batches using the youTuvbe video).
+# https://www.youtube.com/watch?v=HjeW6ed2dmI
 
 
